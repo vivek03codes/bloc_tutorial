c4fc17cd7c3e6a2e8ab9d0777c81c84db0a10184
Vivek Yadav <g21cs.vivek.yadav@gnkhalsa.edu.in>
2025-10-09 23:28:36 +0530

    Initial commit

---
+++ lib/counter/view/counter_view.dart
@@ -0,0 +1,37 @@
+import 'package:bloc_tutorial/counter/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterView extends StatelessWidget {
+  const CounterView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: BlocBuilder<CounterCubit, int>(
+            builder: (context, state) {
+              return Text("$state");
+            },
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            key: const Key("counterView_increment_floatingActionButton"),
+              onPressed: () => context.read<CounterCubit>().increment(),
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 8),
+          FloatingActionButton(
+            key: const Key('counterView_decrement_floatingActionButton'),
+            child: const Icon(Icons.remove),
+            onPressed: () => context.read<CounterCubit>().decrement(),
+          ),
+        ],
+      ),
+    );
+  }
+}
